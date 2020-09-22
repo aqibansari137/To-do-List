c9bd07a652a289e572f2a931ce5a8bcb3cc4c7e7
aqib ansari <aqibansari137@gmail.com>
2020-09-22 09:31:28 +0530

    Updates

---
+++ precache-manifest.c4f1b1a8f53e07c3b688e093a8852ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4a5b69ba2bf99aba24298cc29077f2",
+    "url": "/To-do-List/index.html"
+  },
+  {
+    "revision": "1857dc417cfc42929119",
+    "url": "/To-do-List/static/css/main.a99200be.chunk.css"
+  },
+  {
+    "revision": "ac82b6525046c4cf7ee5",
+    "url": "/To-do-List/static/js/2.312d7260.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-List/static/js/2.312d7260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1857dc417cfc42929119",
+    "url": "/To-do-List/static/js/main.d2a3578d.chunk.js"
+  },
+  {
+    "revision": "81be87ba53c71dff8425",
+    "url": "/To-do-List/static/js/runtime-main.04e17206.js"
+  }
+]);
